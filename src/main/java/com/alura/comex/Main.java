8c3d1d9fc689741823516cf5075f406d4461c7e1
0303 <wilson.osorio@uptc.edu.co>
2025-01-16 06:47:38 -0500

    Some methods were refactored in the class InformeSintetico

--- src/main/java/com/alura/comex/Main.java
+++ src/main/java/com/alura/comex/Main.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
